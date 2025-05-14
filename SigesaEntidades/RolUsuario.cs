14b45aacb3b26db27f7193580067272909152b62
Abner David Dotel Chalas <dotelchalas@gmail.com>
2025-05-14 09:23:53 -0400

    Agregar archivos de proyecto.

---
+++ SigesaEntidades/RolUsuario.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace SigesaEntidades
+{
+    public class RolUsuario
+    {
+        public int IdRolUsuario { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string FechaCreacion { get; set; } = null!;
+    }
+}
